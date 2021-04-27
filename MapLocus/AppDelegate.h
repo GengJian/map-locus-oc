4ec60721d4d7f332ca4cdaef43ddea84845ca927
Soul <soulstayreal@gmail.com>
2021-04-27 15:26:36 +0800

    Initial Commit

---
+++ MapLocus/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MapLocus
+//
+//  Created by Soul on 2021/4/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MapLocus/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MapLocus
+//
+//  Created by Soul on 2021/4/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
